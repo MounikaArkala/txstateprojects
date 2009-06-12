38ab92ac2385043b8d85cfedeecbf56575ae0490
rabidpoobear <rabidpoobear@6fadd0d4-4f45-11de-825b-250a7aa74ab4>
2009-06-12 03:45:08 +0000

    primes fixed, starting work on scales.py

--- trunk/scales.py
+++ trunk/scales.py
@@ -14,13 +14,24 @@ print
 print "<br />Matching Scales<br />"
 
 
+def pretty(notes, encoding):
+	""" notes is int array, encoding is a dict such as {"A":0, "A#":1 ... "G#":11}"""
+	temp = "    <td class=\"primesdata\">%s</td>\n" * len(notes)
+	return temp % tuple([encoding[i] for i in notes])
+
+
 # Create instance of FieldStorage 
 form = cgi.FieldStorage() 
 
-page = form.getvalue('page')
+#page = form.getvalue('page')
 
 # Get data from fields
 row = form.getvalue('notes')
+
+
+row = "A B C"
+
+
 all_notes = {}
 current_note = 0
 for note in [("A",2),("B",1),("C",2),("D",2),("E",1),("F",2),("G",2)]:
@@ -37,55 +48,6 @@ for note in row.split():
 	encoding[all_notes[note]] = note
 
 	
-rownumerals = [all_notes[i.strip()] for i in row.split()]
-
-
-# store all primes by starting note
-allprimes = []
-for x in range(12):
-	allprimes.append(prime(rownumerals, x))
-
-
-def pretty(notes, encoding):
-	""" notes is int array, encoding is a dict such as {"A":0, "A#":1 ... "G#":11}"""
-	temp = "    <td class=\"primesdata\">%s</td>\n" * len(notes)
-	return temp % tuple([encoding[i] for i in notes])
-
-	
-		
-# primes table
-print "<table class=\"primes\">"
-print "  <tr class=\"primesrow\">"
-#pretty-print columns in correct order.
-print "    <td />"
-print "".join([("    <th class=\"primesheader\">I%s</th>\n" % i)for i in rownumerals])
-print "  </tr>"
-for y in inversion(rownumerals, 0):
-	print "  <tr class=\"primesrow\">"
-	sys.stdout.write(("    <th class=\"primesheader\">P%s</th>\n" % y))
-	print pretty(allprimes[y], encoding)
-	sys.stdout.write(("    <th class=\"primesheader\">R%s</th>\n" % y))
-	print "  </tr>"
-	
-	
-print "    <td />"
-print "".join([("    <th class=\"primesheader\">RI%s</th>\n" % i)for i in rownumerals])
-print "  </tr>"
-print "</table>"
-
-
-if page == "main":	
-
-	print '<br /><form action="od.cgi" method="POST">'
-	print 'Search for an order-dependent series<br />'
-	print '<input type="text" name="series" class="TextInput" />'
-	print '<input type="hidden" name="notes" value="%s" />' % row
-	print '<input type="submit" value="Submit" class="SubmitButton" />'
-	print '</form>'
-
-	print '<br /><form action="oid.cgi" method="POST">'
-	print 'Search for an order-independent series<br />'
-	print '<input type="text" name="series" class="TextInput" />'
-	print '<input type="hidden" name="notes" value="%s" />' % row
-	print '<input type="submit" value="Submit" class="SubmitButton" />'
-	print '</form>'
+notes = [all_notes[i.strip()] for i in row.split()]
+noteset = all_perms(notes)
+print [i for i in noteset]
