92c1338cf7afc80c08a1f416c36ded7d6cf00f29
rabidpoobear <rabidpoobear@6fadd0d4-4f45-11de-825b-250a7aa74ab4>
2009-06-15 02:31:46 +0000

    first upload of NES development.  Rom loader appears to work, no unit tests in place yet.

---
+++ 6502/romload.py
@@ -0,0 +1,60 @@
+class ReservedBitsError(Exception):
+    """Exception raised for errors in checking reserved bits in the NES header."""
+    def __init__(self, value="No message specified."):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class NESHeaderError(Exception):
+    """Exception raised when the ROM doesn't contain NES^Z tag as the first 4 characters."""
+    def __init__(self, value="No message specified."):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+
+class Rom(object):
+
+    def __init__(self, f, validate=False, chk_header=True):
+        """ set validate to True if you want to run checks."""
+        
+        data = open(f, "rb").read()
+        if data[0:4] != "NES" + chr(26):
+            if chk_header:
+                raise NESHeaderError
+        data = [ord(i) for i in data]
+        
+        if data[6] & 0x0D != 0: # bit 1, 2, 3 must be 0s.
+            if validate:
+                raise ReservedBitsError
+        if data[9] & 0xFE != 0: # bits 1-7 must be 0s.
+            if validate:
+                raise ReservedBitsError
+        if sum(data[10:16]) != 0:
+            if validate:
+                raise ReservedBitsError
+            
+        self.rom_banks   = data[4]
+        self.vrom_banks  = data[5]
+        self.vmirror     = data[6] & 0x01 # bit 0, vertical / horizontal mirroring
+        self.batt_ram    = data[6] & 0x02 # bit 1, battery-backed RAM at $6000-$7FFF
+        self.has_trainer = data[6] & 0x04 # bit 2, 512-byte trainer at $7000-$71FF
+        self.four_screen = data[6] & 0x08 # bit 3, four-screen VRAM layout.
+        self.vs_system   = data[7] & 0x01 # bit 0, vertical / horizontal mirroring
+        self.ram         = data[8]
+        self.pal         = data[9] & 0x01
+        self.mapper      = (data[7] & 0xF0) + (data[6] & 0x0F) # because marat is retarded. or a genius.
+
+        if self.ram == 0: # compatibility with older specs of the .NES format
+            self.ram = 1
+
+        startaddr = 16
+        if self.has_trainer:
+            self.trainer = data[startaddr:startaddr+512]
+            startaddr += 512
+
+        temp = data[startaddr:startaddr+self.rom_banks*16384]
+        self.rom = [temp[i*16384:(i+1)*16384] for i in range(self.rom_banks)]
+        temp = data[startaddr+self.rom_banks*16384:]
+        self.vrom = [temp[i*8192:(i+1)*8192] for i in range(self.vrom_banks)]
+
