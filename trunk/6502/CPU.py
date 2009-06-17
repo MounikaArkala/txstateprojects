22cf19f88a0e984b94ffbb0011d9585fe74fc0e4
rabidpoobear <rabidpoobear@6fadd0d4-4f45-11de-825b-250a7aa74ab4>
2009-06-17 12:18:08 +0000

    a bit of playing around w/ CPU, not much.  Added best ref docs so far.

--- trunk/6502/CPU.py
+++ trunk/6502/CPU.py
@@ -1,9 +1,22 @@
+class Interrupt(object):
 
+    def __init__(self, t="NMI"):
+        self.type = t
+
+        
 class Register(object):
 
     def __init__(self, val, size=8):
         self.val = val
         self.size = size
+            
+    def getLow(self, num_bits):
+        return self.val & (2**num_bits - 1)
+        # eg. for 16, 8 will yield (self.val & 0000000011111111)
+    
+    def getHigh(self, num_bits):
+        return self.val & ((2**self.size - 1) - (2**(self.size-num_bits) - 1))
+        # eg. for 16, 8 will yield self.val & 1111111100000000
 
 class UnsignedRegister(Register):
     
@@ -20,29 +33,57 @@ class UnsignedRegister(Register):
             zero = True
             
         return (overflow, zero)
-            
-    def getLow(self, num_bits):
-        """ Tested, works! """
-        return self.val & (2**num_bits - 1)
-        # eg. for 16, 8 will yield (self.val & 0000000011111111)
-    
-    def getHigh(self, num_bits):
-        """ Tested, works! """
-        return self.val & ((2**self.size - 1) - (2**(self.size-num_bits) - 1))
-        # eg. for 16, 8 will yield self.val & 1111111100000000
+
 
 
 class c6502(object):
 
+    def getStatus(self):
+        return (self.negative    << 7) + \
+               (self.overflow    << 6) + \
+               0x20                    + \
+               (self.brk         << 4) + \
+               (self.decimal     << 3) + \
+               (self.int_disable << 2) + \
+               (self.zero        << 1) + \
+               self.carry
+
+    PS = property(getStatus) # processor status
+
     def __init__(self):
 
-        self.X   = UnsignedRegister(0)
-        self.Y   = UnsignedRegister(0)
-        self.S   = UnsignedRegister(0)
-        self.ACC = UnsignedRegister(0)
-        self.PC  = UnsignedRegister(0,16)
-        
+        self.X  = UnsignedRegister(0)
+        self.Y  = UnsignedRegister(0)
+        self.S  = UnsignedRegister(0) # stack pointer
+        self.A  = UnsignedRegister(0) #accumulator
+        self.PC = UnsignedRegister(0,16)
 
+        self.negative    = 0
+        self.overflow    = 0
+        self.carry       = 0
+        self.zero        = 0
+        self.int_disable = 0
+        self.decimal     = 0
+        self.brk         = 0
+
+        
+#Load / Store Operations - Load a register from memory or stores the contents of a register to memory. 
+#Register Transfer Operations - Copy contents of X or Y register to the accumulator or copy contents of accumulator to X or Y register. 
+#Stack Operations - Push or pull the stack or manipulate stack pointer using X register. 
+#Logical Operations - Perform logical operations on the accumulator and a value stored in memory. 
+#Arithmetic Operations - Perform arithmetic operations on registers and memory. 
+#Increments / Decrements - Increment or decrement the X or Y registers or a value stored in memory. 
+# Shifts - Shift the bits of either the accumulator or a memory location one bit to the left or right. 
+ #Jumps / Calls - Break sequential execution sequence, resuming from a specified address. 
+ #Branches - Break sequential execution sequence, resuming from a specified address, if a condition is met. The condition involves examining a specific bit in the status register. 
+ #Status Register Operations - Set or clear a flag in the status register. 
+ #System Functions - Perform rarely used functions. 
+ 
+
+
+x = c6502()    
+x.carry = 1
+print x.PS
 """
 Counter=InterruptPeriod;
 PC=InitialPC;
