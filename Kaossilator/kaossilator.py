228e29a3eb4f62b3b995c40fbd6fa31575b8180d
rabidpoobear <rabidpoobear@6fadd0d4-4f45-11de-825b-250a7aa74ab4>
2009-06-19 10:41:18 +0000

    added the python_supercollider library to the project, no significant progress made.

--- Kaossilator/kaossilator.py
+++ Kaossilator/kaossilator.py
@@ -2,33 +2,48 @@
 
 import pygame
 from pygame.locals import *
-import random
+import random, scaleslib
 #import Numeric
 from math import *
 
-WIDTH = 320     #width of screen
-HEIGHT = 240    #height of screen
+WIDTH = 800    #width of screen
+HEIGHT = 480    #height of screen
+
+import sc
+import time, os
+
+
 
     
 def main():
     pygame.display.init()
     
     screen = pygame.display.set_mode((WIDTH,HEIGHT),DOUBLEBUF,32)
-    pixels = pygame.surfarray.pixels3d(screen)
-    
-    width = len(pixels)-1
-    height = len(pixels[0])-1
-    freq = 100.0
-    
-    for y in xrange(height):
-        for x in xrange(width):
-            z1 = sin(x/freq*1.7*pi)
-            z2 = sin((x/3+y)/freq*1.5*pi)
-            z3 = sin(y/freq*0.1*pi)
-            
-            z = abs(z1+z2+z3)*255
-            pixels[x,y] = (z,z/4,z*4)
 
+        
+    # we need to tell python where the scserver application lives, this
+    # changes between platforms and systems. You might need to edit it.
+    # exedir = 'usr/local' # where does scsynth live?
+    # sc.start(exedir, verbose=1, spew=1 )
+
+    sc.start()
+
+    player = sc.Synth( "StereoPlayer" )
+
+    player.bufnum = sc.loadSnd( "numeros.wav", wait=True )
+
+    print "loading bufnum with ID", player.bufnum
+
+    time.sleep(5) # wait while sound plays
+
+    sc.unloadSnd( player.bufnum )
+    player.free()
+    sc.quit()
+
+    print 'quiting'
+
+
+        
     pygame.display.update()
     done = False
     while not done:
