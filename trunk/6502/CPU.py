728445716b3e6af7e448093e3dd2222aa4c7d711
rabidpoobear <rabidpoobear@6fadd0d4-4f45-11de-825b-250a7aa74ab4>
2009-06-25 13:18:03 +0000

    25 opcodes left out of 56.  All current opcodes have extensive unit tests.  all current unit tests pass.  all opcodes so far are cycle-accurate and appear to work, no known bugs at the current time.
    no interrupts currently programmed (break, VBlank, etc.), only a very primitive calling of functions is implemented.

--- trunk/6502/CPU.py
+++ trunk/6502/CPU.py
@@ -1,44 +1,77 @@
+
+def debug(astr):
+    print "\n%s\n" % astr
+
+
 class Interrupt(object):
 
     def __init__(self, t="NMI"):
         self.type = t
 
         
-class Register(object):
-
-    def __init__(self, val, size=8):
-        self.val = val
-        self.size = size
-            
-    def getLow(self, num_bits):
-        return self.val & (2**num_bits - 1)
-        # eg. for 16, 8 will yield (self.val & 0000000011111111)
-    
-    def getHigh(self, num_bits):
-        return self.val & ((2**self.size - 1) - (2**(self.size-num_bits) - 1))
-        # eg. for 16, 8 will yield self.val & 1111111100000000
-
-class UnsignedRegister(Register):
-    
-    def add(self, val):
-        """ add a value to the register.  returns a set of flags (zero, overflow)"""
-        self.val += val
-        zero = False
-        overflow = False
         
-        if self.val > 2**self.size:
-            self.val -= 2**self.size
-            overflow = True
-        if self.val == 0:
-            zero = True
             
-        return (overflow, zero)
+def getLow(val, num_bits, size=16):
+    return val & (2**num_bits - 1)
+    # eg. for 16, 8 will yield (val & 0000000011111111)
+    
+def getHigh(val, num_bits, size=16):
+    return val & ((2**size - 1) - (2**(size-num_bits) - 1))
+    # eg. for 16, 8 will yield val & 1111111100000000
 
+def unsignedAdd(val, inc, size=8):
+    """ add a value to the register.  returns a set of flags (negative, zero, overflow)"""
+    negative = val >> 7
+    
+    val += inc
+        
+    zero = 0
+    negative = 0
+    
+    if val >= 2**size:
+        val -= 2**size
+        
+    elif val < 0:
+        val = (2**size) + inc
+        
+        
+    if val == 0:
+        zero = 1
+        
+    if not negative and (val >> 7): #nonnegative overflowed into negative bit.
+        overflow = 1
+    else:
+        overflow = 0
+        
+    if negative and (val >> 7) == 0: #negative overflowed into positive.  #TODO: is this right?
+        overflow = 1
+    else:
+        overflow = 0
+    
+    negative = val >> 7
+        
+    return (val, negative, overflow, zero)
 
+def unSign(val, size=8): #takes in a 2's complement number, returns a non-complemented form.
+    if val >> (size-1):# need to take 2's complement
+        temp = 2**size - 1
+        val ^= temp #XOR to flip bits
+        val += 1 # add 1
+        val = 0 - val
+        return val
+        
+    else: #just return val as-is
+        return val
+        
 
+        
+        
+        
+        
+        
 class c6502(object):
 
-    def getStatus(self):
+    def _getStatus(self):
         return (self.negative    << 7) + \
                (self.overflow    << 6) + \
                0x20                    + \
@@ -47,56 +80,308 @@ class c6502(object):
                (self.int_disable << 2) + \
                (self.zero        << 1) + \
                self.carry
-
-    PS = property(getStatus) # processor status
+               
+    def _setStatus(self, val):
+        self.negative    = (val & 0x80) >> 7
+        self.overflow    = (val & 0x40) >> 6
+        self.brk         = (val & 0x10) >> 4
+        self.decimal     = (val & 0x08) >> 3
+        self.int_disable = (val & 0x04) >> 2
+        self.zero        = (val & 0x02) >> 1
+        self.carry       = (val & 0x01)
+    
+    
+    PS = property(_getStatus, _setStatus) # processor status
+    
 
     def __init__(self):
-
-        self.X  = UnsignedRegister(0)
-        self.Y  = UnsignedRegister(0)
-        self.S  = UnsignedRegister(0) # stack pointer
-        self.A  = UnsignedRegister(0) #accumulator
-        self.PC = UnsignedRegister(0,16)
-
-        self.negative    = 0
-        self.overflow    = 0
-        self.carry       = 0
-        self.zero        = 0
-        self.int_disable = 0
-        self.decimal     = 0
-        self.brk         = 0
-        self.cycles      = 0
+        #optable is of form Opcode: (function, function_parameters(usually addressing mode))
+        self.optable = { \
         
-        self.clear_memory()
+        #Branches
+        0xB0 : ("Branch Carry Set",      "BCS", self.BCS, None),
+        0x90 : ("Branch Carry Clear",    "BCC", self.BCC, None),
+        0xF0 : ("Branch Zero Set",       "BEQ", self.BEQ, None),
+        0xD0 : ("Branch Zero Clear",     "BNE", self.BNE, None),
+        0x30 : ("Branch Negative Set",   "BMI", self.BMI, None),
+        0x10 : ("Branch Negative Clear", "BPL", self.BPL, None),
+        0x70 : ("Branch Overflow Set",   "BVS", self.BVS, None),
+        0x50 : ("Branch Overflow Clear", "BVC", self.BVC, None),
+        
+        
+        
+        #Register Transfer Ops
+        0x9A : ("Transfer X to S",       "TXS", self.TXS, None),
+        0xBA : ("Transfer S to X",       "TSX", self.TSX, None),
+        0x8A : ("Transfer X to A",       "TXA", self.TXA, None),
+        0xAA : ("Transfer A to X",       "TAX", self.TAX, None),
+        0x98 : ("Transfer Y to A",       "TYA", self.TYA, None),
+        0xA8 : ("Transfer A to Y",       "TAY", self.TAY, None),
+        
+        #Stack Ops
+        0x48 : ("Push A",                "PHA", self.PHA, None),
+        0x08 : ("Push Processor Status", "PHP", self.PHP, None),
+        0x68 : ("Pull A",                "PLA", self.PLA, None),
+        0x28 : ("Pull Processor Status", "PLP", self.PLP, None),
+        
+        #Status Register Ops
+        0x18 : ("Clear Carry",        "CLC", self.CLC, None),
+        0xD8 : ("Clear Decimal",      "CLD", self.CLD, None),
+        0x58 : ("Clear Int. Disable", "CLI", self.CLI, None),
+        0xB8 : ("Clear Overflow",     "CLV", self.CLV, None),
+        0x38 : ("Set Carry",          "SEC", self.SEC, None),
+        0xF8 : ("Set Decimal",        "SED", self.SED, None),
+        0x78 : ("Set Int. Disable",   "SEI", self.SEI, None),
+        
+        #Increments / Decrements
+        0xE8 : ("Increment X",        "INX", self.INX, None),
+        0xC8 : ("Increment Y",        "INX", self.INY, None),
+        0xCA : ("Decrement X",        "DEX", self.DEX, None),
+        0x88 : ("Decrement Y",        "DEY", self.DEY, None),
+        
+        #Special Ops
+        0xEA : ("No Operation",       "NOP", self.NOP, None),
+        }
+        
+        self.reset()
 
     def clear_memory(self):
-        self.memory = [0]*8000
+        self.memory = [0]*0x10000
 
     def step(self):
-        opcode = self.memory[self.PC]
-        if opcode == 0x18:
-            self.CLC()
+        opcode = self.read(self.PC)
+        self.PC += 1
+        try:
+            op = self.optable[opcode]
+            debug("executing a %s" % op[0])
+            if (op[3]):
+                op[2](op[3])
+            else:
+                op[2]()
+                
+        except KeyError:
+            debug("Invalid OPCODE (%s)" % opcode)
+            self.UnsupportedOpcode()
+            #TODO: raise UnsupportedOpcode exception here
             
+    def reset(self):
+        self.clear_memory() # init memory
+        self.X      = 0     # X index register
+        self.Y      = 0     # Y index register
+        self.A      = 0     # Accumulator
+        self.S      = 0xFF  # Stack Pointer
+        self.PC     = 0     # Program Counter
+        self.PS     = 0     # Processor Status
+        self.cycles = 0     # Cycle Count
+
+    def write(self, addr, value):
+        """ writes a value to a memory location.
+        also handles any interrupts / mirroring that may 
+        need to be taken care of involvind that address."""
+        self.memory[addr] = value
+
+    def read(self, addr):
+        return self.memory[addr]
 
     #OPCODES
-
+    
+    
+    #Register Transfer Operations - Copy contents of X or Y register to the accumulator or copy contents of accumulator to X or Y register. 
+    
+    def TXS(self):
+        self.S = self.X
+        self.cycles += 2
+        
+    def TSX(self):
+        self.X = self.S
+        self.negative = self.X >> 7
+        if self.X == 0:
+            self.zero = 1
+        else:
+            self.zero = 0
+        self.cycles += 2
+        
+    def TXA(self):
+        self.A = self.X
+        self.negative = self.A >> 7
+        if self.A == 0:
+            self.zero = 1
+        else:
+            self.zero = 0
+        self.cycles += 2
+        
+    def TAX(self):
+        self.X = self.A
+        self.negative = self.A >> 7
+        if self.A == 0:
+            self.zero = 1
+        else:
+            self.zero = 0
+        self.cycles += 2
+        
+    def TYA(self):
+        self.A = self.Y
+        self.negative = self.A >> 7
+        if self.A == 0:
+            self.zero = 1
+        else:
+            self.zero = 0
+        self.cycles += 2
+        
+    def TAY(self):
+        self.Y = self.A
+        self.negative = self.A >> 7
+        if self.A == 0:
+            self.zero = 1
+        else:
+            self.zero = 0
+        self.cycles += 2
+    
+    
+    #Logical Operations - Perform logical operations on the accumulator and a value stored in memory.
+    
+    # Shifts - Shift the bits of either the accumulator or a memory location one bit to the left or right. 
+    
+    
+    #Stack Operations - Push or pull the stack or manipulate stack pointer using X register. 
+    def PHA(self):
+        self.write(self.S + 0x100, self.A)
+        self.S -= 1
+        if self.S < 0:
+            self.S = 0 #TODO: raise exception?
+        self.cycles += 3
+        
+    def PHP(self):
+    
+        self.write(self.S + 0x100, self.PS)
+        self.S -= 1
+        if self.S < 0:
+            self.S = 0 #TODO: raise exception?
+        self.cycles += 3
+        
+    def PLA(self):
+        self.S += 1
+        if self.S > 0xFF:
+            self.S = 0xFF #TODO: raise exception?
+        self.A = self.read(self.S + 0x100)
+        self.negative = self.A >> 7 # bit 7 is negative bit
+        if self.A == 0:
+            self.zero = 1
+        else:
+            self.zero = 0
+        self.cycles += 4
+        
+    def PLP(self):
+        self.S += 1
+        if self.S > 0xFF:
+            self.S = 0xFF #TODO: raise exception?
+        self.PS = self.read(self.S + 0x100)
+        self.cycles += 4
+    
+    
+    #Arithmetic Operations - Perform arithmetic operations on registers and memory. 
  
-
+ 
+    #Load / Store Operations - Load a register from memory or stores the contents of a register to memory. 
+    #Jumps / Calls - Break sequential execution sequence, resuming from a specified address. 
+    
+    
+    #Branches - Break sequential execution sequence, resuming from a specified address, if a condition is met. The condition involves examining a specific bit in the status register.
+    def _BRANCH(self, val, comp):
+        """ Compare val with comp, and if they're equal, do a branch."""
+        displacement = self.read(self.PC)
+        self.PC += 1
+        self.cycles += 2
+        if val == comp:
+            self.cycles += 1
+            dest = self.PC + unSign(displacement)
+            if (self.PC / 256) != (dest / 256):
+                self.cycles += 1 #page boundary.
+            self.PC = dest
+        
+    def BCC(self):
+        self._BRANCH(self.carry, 0)
+        
+    def BCS(self):
+        self._BRANCH(self.carry, 1)
+        
+    def BEQ(self):
+        self._BRANCH(self.zero, 1)
+        
+    def BNE(self):
+        self._BRANCH(self.zero, 0)
+            
+    def BMI(self):
+        self._BRANCH(self.negative, 1)
+        
+    def BPL(self):
+        self._BRANCH(self.negative, 0)
+        
+    def BVC(self):
+        self._BRANCH(self.overflow, 0)
+        
+    def BVS(self):
+        self._BRANCH(self.overflow, 1)
+        
+ 
+    #Increments / Decrements - Increment or decrement the X or Y registers or a value stored in memory. 
+    def INX(self):
+        self.X, self.negative, trash, self.zero = unsignedAdd(self.X, 1)
+        self.cycles += 2
+        
+    def DEX(self):
+        self.X, self.negative, trash, self.zero = unsignedAdd(self.X, -1)
+        self.cycles += 2
+        
+    def INY(self):
+        self.Y, self.negative, trash, self.zero = unsignedAdd(self.Y, 1)
+        self.cycles += 2
+        
+    def DEY(self):
+        self.Y, self.negative, trash, self.zero = unsignedAdd(self.Y, -1)
+        self.cycles += 2
+         
+    
     #Status Register Operations - Set or clear a flag in the status register. 
     def CLC(self):
         self.carry = 0
         self.cycles += 2
+
+    def CLD(self):
+        self.decimal = 0
+        self.cycles += 2
+
+    def CLI(self):
+        self.int_disable = 0
+        self.cycles += 2
         
+    def CLV(self):
+        self.overflow = 0
+        self.cycles += 2
+
+    def SEC(self):
+        self.carry = 1
+        self.cycles += 2
         
-#Load / Store Operations - Load a register from memory or stores the contents of a register to memory. 
-#Register Transfer Operations - Copy contents of X or Y register to the accumulator or copy contents of accumulator to X or Y register. 
-#Stack Operations - Push or pull the stack or manipulate stack pointer using X register. 
-#Logical Operations - Perform logical operations on the accumulator and a value stored in memory. 
-#Arithmetic Operations - Perform arithmetic operations on registers and memory. 
-#Increments / Decrements - Increment or decrement the X or Y registers or a value stored in memory. 
-# Shifts - Shift the bits of either the accumulator or a memory location one bit to the left or right. 
- #Jumps / Calls - Break sequential execution sequence, resuming from a specified address. 
- #Branches - Break sequential execution sequence, resuming from a specified address, if a condition is met. The condition involves examining a specific bit in the status register.
+    def SED(self):
+        self.decimal = 1
+        self.cycles += 2
+        
+    def SEI(self):
+        self.int_disable = 1
+        self.cycles += 2
+        
+    #Special Operations
+    
+    def BRK(self):
+        pass
+        
+    def NOP(self):
+        self.cycles += 2
+        
+    def UnsupportedOpcode(self):
+        pass #TODO: raise UnsupportedOpcode exception here
+        
  #System Functions - Perform rarely used functions. 
  
 if __name__ == "__Main__":
@@ -104,6 +389,7 @@ if __name__ == "__Main__":
     x = c6502()    
     x.carry = 1
     print x.PS
+    
 """
 Counter=InterruptPeriod;
 PC=InitialPC;
