a5c9d051caa5f0adf3abd09f178908db46e12f4a
rabidpoobear <rabidpoobear@6fadd0d4-4f45-11de-825b-250a7aa74ab4>
2009-08-10 10:16:48 +0000

    converted to non-ajax solution, also made multi-row scales in scales program.  scales program reimplemented in C++ for speed.  much faster now.

---
+++ MusicAnalysis/libs/structure.py
@@ -0,0 +1,16 @@
+def print_header(title="Music Analysis", css=[], scripts=[]):
+    css_txt = "".join([('<link rel="stylesheet" type="text/css" href="stylesheets/%s" />\n' % s) for s in css])
+    script_txt = "".join([('<script src="scripts/%s"></script>\n' % s) for s in scripts])
+    subs = {}
+    subs['title'] = title
+    subs['scriptlinks'] = css_txt + script_txt
+    print "Content-type: text/html"
+    print
+    print (open("main/header.html").read() % subs)
+    
+def print_footer():
+    print open("main/footer.html").read()
+
+
+def print_body(filename):
+    print open(filename).read()
