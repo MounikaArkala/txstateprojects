f6ae360835bb2ae515a745442c7fad76f04a0013
rabidpoobear <rabidpoobear@6fadd0d4-4f45-11de-825b-250a7aa74ab4>
2010-02-04 09:26:06 +0000

    add project 1 (SCANNER) for compilation class

---
+++ trunk/Spring 2009 notes/Crafting Compilers (5531)/Project 1/test.c
@@ -0,0 +1,22 @@
+ifasds
+
+int main()
+{
+   int a = 4, b = 5;
+   while (1)
+   {
+       printf("Hello, \"world!\"");
+       /* this is a
+       multiline comment.*/
+       if (a > b)
+       {
+            printf("A is greater!\n");
+       }
+       else
+       {
+           printf("B is greater!\t");
+       }
+       
+       printf("The world is \\");
+   }
+}
