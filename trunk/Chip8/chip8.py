e0b2416d8e94b61417a49da84e3d4328cffeced3
rabidpoobear <rabidpoobear@6fadd0d4-4f45-11de-825b-250a7aa74ab4>
2010-04-01 02:57:44 +0000

    added characters.

--- trunk/Chip8/chip8.py
+++ trunk/Chip8/chip8.py
@@ -11,8 +11,14 @@ class CPU(object):
     def __init__(self):
         self.memory = [0] * 0xFFFF
         #TODO: load the characters into memory.
+        self.memory
+        
+        self.screen = [[0] * 0x20 for i in range(0x40)]
+        self.screen[3][2]  = 1
+        print self.screen
         
         
+    def draw(self, surface):
         
         
 cpu = CPU()
---
+++ trunk/Chip8/extract-chars.py
@@ -0,0 +1,8 @@
+
+items = []
+
+for line in open('chars.txt'):
+    if line.strip().startswith('0x'):
+        items.append(int(line[2:],16))
+print len(items)
+print items
