5bd483586710b214202fdfeccdc4c556a856a433
rabidpoobear <rabidpoobear@6fadd0d4-4f45-11de-825b-250a7aa74ab4>
2010-04-08 21:55:55 +0000

    added Display

--- Chip8/extract-chars.py
+++ Chip8/extract-chars.py
@@ -1,8 +1,6 @@
 
 items = []
-
 for line in open('chars.txt'):
     if line.strip().startswith('0x'):
         items.append(int(line[2:],16))
-print len(items)
 print items
---
+++ Chip8/main.py
@@ -0,0 +1,3 @@
+import chip8
+
+
